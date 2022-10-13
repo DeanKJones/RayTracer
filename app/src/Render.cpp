c52de27da1f1bc9e0334afb8b9646283878bd772
Dean <dhkjones@gmail.com>
2022-10-13 14:59:35 +0200

    Remove Error Comment, my math was actually right :)

--- app/src/Render.cpp
+++ app/src/Render.cpp
@@ -121,8 +121,6 @@ glm::vec3 Renderer::RenderColor(Ray& ray, int depth)
 
         if (payload.materialPtr->scatter(ray, payload, attenuation, scattered))
         {
-            // Incorrect attenuation value, multiplying by a fall off over time makes cleaner images. 
-            // Attenuation should decrease over bounces?
             RayHitColor = (attenuation * RenderColor(scattered, depth - 1)) * 0.75f;
             return RayHitColor;
         }
