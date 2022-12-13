e639d0f3b68686a16a17afdd891715517c831c03
Dean <dhkjones@gmail.com>
2022-12-13 16:44:45 +0100

    PBRT.3 parallel class implementation

---
+++ core/src/Parallel.cpp
@@ -0,0 +1,102 @@
+#include "Parallel.h"
+
+/* Local Definitions */
+static std::vector<std::thread> threads;
+static bool shutdownThreads = false;
+
+extern thread_local int tl_threadIndex;
+
+static parallelForLoop *workList = nullptr;
+static std::mutex workListMutex;
+static std::condition_variable workListCondition;
+
+
+static void workerThreadFunc(int tIndex) {
+    tl_threadIndex = tIndex;
+    std::unique_lock<std::mutex> lock(workListMutex);
+    while(!shutdownThreads){
+
+        // Sleep until there are more tasks
+        if (!workList){
+            workListCondition.wait(lock);
+        }
+        // Get work from workList and run iterations
+        else {
+            parallelForLoop &loop = *workList;
+
+            // Find the set of loop iterations to run next
+            int64_t indexStart = loop.nextIndex;
+            int64_t indexEnd = std::min(indexStart + loop.chunkSize, loop.maxIndex);
+
+            // Update loop to iteration that this thread will run
+            loop.nextIndex = indexEnd;
+            if (loop.nextIndex == loop.maxIndex) {
+                workList = loop.next;
+            }
+            loop.activeWorkers++;
+
+            lock.unlock();
+            for (int index = indexStart; index < indexEnd; ++index) {
+                // If function in the case that there is another type of loop
+                if (loop.func1D) {
+                    loop.func1D(index);
+                }
+                lock.lock();
+            }
+            if (loop.Finished())
+                workListCondition.notify_all();
+        }
+    }
+    //ReportThreadStatus();
+}
+
+
+void parallelFor(std::function<void(int)> func, int64_t count, int chunkSize)
+{
+    Renderer renderer;
+    if (renderer.GetSettings().threadCount == 1 || count < chunkSize) {
+        for(int i = 0; i < count; ++i)
+            func(i);
+        return;
+    }
+
+    if (threads.size() == 0 ) {
+        tl_threadIndex = 0;
+        for (int i = 0; i < numSystemCores() - 1; ++i){
+            threads.push_back(std::thread(workerThreadFunc, i + 1));
+        }
+    }
+
+    parallelForLoop loop(func, count, chunkSize);
+
+    workListMutex.lock();
+    loop.next = workList;
+    workList = &loop;
+    workListMutex.unlock();
+
+    // Notify worker threads of work
+    std::unique_lock<std::mutex> lock(workListMutex);
+    workListCondition.notify_all();
+
+    while (!loop.Finished())
+    {
+        int64_t indexStart = loop.nextIndex;
+        int64_t indexEnd = std::min(indexStart + loop.chunkSize, loop.maxIndex);
+
+        loop.nextIndex = indexEnd;
+        if (loop.nextIndex == loop.maxIndex){
+            workList = loop.next;
+        }
+        loop.activeWorkers++;
+
+        lock.unlock();
+        for (int index = indexStart; index < indexEnd; ++index) {
+            if (loop.func1D) {
+                loop.func1D(index);
+            }
+        }
+        lock.lock();
+
+        loop.activeWorkers--;
+    }
+}
---
+++ core/src/Parallel.h
@@ -0,0 +1,32 @@
+#ifndef RAYTRACING_PARALLEL_H
+#define RAYTRACING_PARALLEL_H
+
+#include <iostream>
+#include "thread"
+
+#include "../../app/src/Render.h"
+
+class parallelForLoop
+{
+public:
+    parallelForLoop(std::function<void(int)> func1D, int64_t maxIndex, int chunkSize) :
+            func1D(std::move(func1D)), maxIndex(maxIndex),
+            chunkSize(chunkSize) { }
+
+public:
+    std::function<void(int)> func1D;
+
+    const int64_t maxIndex;
+    const int chunkSize;
+
+    int64_t nextIndex = 0;
+    int activeWorkers = 0;
+
+    parallelForLoop *next = nullptr;
+
+    bool Finished() const { return nextIndex >= maxIndex && activeWorkers == 0; }
+};
+
+int numSystemCores() { return std::thread::hardware_concurrency(); }
+
+#endif //RAYTRACING_PARALLEL_H
