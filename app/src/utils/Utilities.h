752cc5cbc031fafbb60a9a41045e6b6788ac3b0d
Dean <dhkjones@gmail.com>
2023-01-05 18:35:03 +0100

    Fix pointer error

--- app/src/utils/Utilities.h
+++ app/src/utils/Utilities.h
@@ -2,13 +2,11 @@
 #include "../obj/Object.h"
 
 // Listbox Object Getter
-bool sphereGetter(void* data, int index, const char** output)
+bool objectGetter(void* data, int index, const char** output)
 {
-    // When using Object* sphere names render incorrectly
-    Sphere* objects = (Sphere*)data;
-    Sphere& currentObject = objects[index];
-
-    *output = currentObject.objectName.c_str();
+    Object** objects = (Object**)data;
+//    Object currentObject = objects[index];
+    *output = objects[index]->objectName.c_str();
 
     return true;
 }
