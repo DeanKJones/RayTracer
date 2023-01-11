a22ad901e20dc0059b200be84eacffe918441cc7
Dean <dhkjones@gmail.com>
2023-01-11 22:39:13 +0100

    Add a magnifying glass to view pixels on rendered images

---
+++ app/src/utils/Magnifier.h
@@ -0,0 +1,15 @@
+#ifndef RAYTRACING_MAGNIFIER_H
+#define RAYTRACING_MAGNIFIER_H
+
+#include <iostream>
+
+#include "src/Image.h"
+#include "glm/glm.hpp"
+
+// Magnifying glass
+std::shared_ptr<Core::Image> magnify(std::shared_ptr<Core::Image> image, glm::vec2 cursorPos)
+{
+
+}
+
+#endif //RAYTRACING_MAGNIFIER_H
