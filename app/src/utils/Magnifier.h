8670c28ae6307a467e1d0369a0287687ecd84b38
Dean <dhkjones@gmail.com>
2023-02-03 10:50:16 +0100

    Clean cursor position float

--- app/src/utils/Magnifier.h
+++ app/src/utils/Magnifier.h
@@ -34,9 +34,9 @@ void MagnifingGlass(std::shared_ptr<Core::Image> image, ImVec2 cursorPos)
     ImGui::Image(image->GetDescriptorSet(), ImVec2(128, 128), uv0, uv1);
 
     // Here we can define different pixel data to write to the ui
-    ImGui::Text("Center: %f.1", fabs(center.x));
+    ImGui::Text("Center: %.0f", fabs(center.x));
     ImGui::SameLine();
-    ImGui::Text("Center: %f.1", fabs(center.y));
+    ImGui::Text(", %.0f", fabs(center.y));
 
     ImGui::End();
     ImGui::PopStyleVar();
