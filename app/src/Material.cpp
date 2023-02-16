eff15f4ee80e9c7722b2a91ddf8604669476e38d
Dean <dhkjones@gmail.com>
2023-02-16 16:26:06 +0100

    Implement Dielectric materials and solve rendering bug

--- app/src/Material.cpp
+++ app/src/Material.cpp
@@ -65,23 +65,14 @@ bool Metal::scatter(
 }
 
 // DIELECTRIC MATERIALS
-/*
- * TODO: Solve sometime
- * First render after the scene's init will render glass incorrectly
- * rendering the scene again (with or without changes) will then correct the glass material
- */
 
 bool Dielectric::scatter(
     const Ray &ray, const Payload &payload, glm::vec3 &colorAttenuation, Ray &scattered) const
 {
     colorAttenuation = albedo;
-
-    // eta = srcIOR / destIOR
-    //float eta = payload.frontFace ? (1.0f / indexOfRefraction) : indexOfRefraction;
-    float eta = 1.0f;
+    float eta = payload.frontFace ? (1.0f / indexOfRefraction) : indexOfRefraction;
 
     glm::vec3 incidentRay = glm::normalize(ray.Direction);
-    //glm::vec3 incidentRay = glm::abs(ray.Direction);
 
     double cos_theta = fmin(glm::dot(incidentRay, payload.worldNormal), 1.0f);
     double sin_theta = glm::sqrt(1.0f - cos_theta * cos_theta);
@@ -90,21 +81,32 @@ bool Dielectric::scatter(
     glm::vec3 direction;
 
     if (cannot_refract || reflectance(cos_theta, eta) > random()) {
-    //if (cannot_refract) {
         direction = reflect(incidentRay, payload.worldNormal);
     } else {
-        //refract(unitDirection, payload.worldNormal, refraction_ratio, direction);
-        //direction = refract2(unitDirection, payload.worldNormal, refraction_ratio);
-        refract3(incidentRay, payload.worldNormal, eta, direction);
+        direction = refract(incidentRay, payload.worldNormal, eta);
     }
 
-    scattered.Origin = payload.hitPosition;
+    // Move the origin forward inside the glass
+    scattered.Origin = payload.hitPosition + (direction * 0.00005f);
     scattered.Direction = direction;
 
     return true;
 }
 
-void Dielectric::refract(const glm::vec3 &incident, const glm::vec3 &normal, float &ior, glm::vec3 &refracted) const {
+glm::vec3 Dielectric::refract(const glm::vec3& uv, const glm::vec3& n, float etai_over_etat) const
+{
+    float cos_theta = fmin(glm::dot(-uv, n), 1.0f);
+
+    glm::vec3 r_out_perp =  etai_over_etat * (uv + cos_theta * n);
+    float r_o_p_lengthSq = (r_out_perp.x * r_out_perp.x) +
+                           (r_out_perp.y * r_out_perp.y) +
+                           (r_out_perp.z * r_out_perp.z);
+    glm::vec3 r_out_parallel = -glm::sqrt(fabs(1.0f - r_o_p_lengthSq)) * n;
+
+    return r_out_perp + r_out_parallel;
+}
+
+void Dielectric::refract_2(const glm::vec3 &incident, const glm::vec3 &normal, float &ior, glm::vec3 &refracted) const {
     float eta = ior;
     float eta2 = eta * eta;
 
@@ -123,17 +125,7 @@ void Dielectric::refract(const glm::vec3 &incident, const glm::vec3 &normal, flo
                     eta * (incident - glm::dot(incident, normal) * normal);*/
 }
 
-glm::vec3 Dielectric::refract2(const glm::vec3& uv, const glm::vec3& n, float etai_over_etat) const
-{
-    float cos_theta = fmin(glm::dot(-uv, n), 1.0f);
-
-    glm::vec3 r_out_perp =  etai_over_etat * (uv + cos_theta * n);
-    glm::vec3 r_out_parallel = -glm::sqrt(glm::abs(1.0f - glm::length(r_out_perp))) * n;
-
-    return r_out_perp + r_out_parallel;
-}
-
-glm::vec3 Dielectric::refract3(glm::vec3 i, glm::vec3 n, float eta, glm::vec3 outRay) const
+glm::vec3 Dielectric::refract_3(glm::vec3 i, glm::vec3 n, float eta, glm::vec3 outRay) const
 {
     glm::vec3 normal = glm::normalize(n);
 
--- app/src/Material.h
+++ app/src/Material.h
@@ -76,9 +76,9 @@ public:
             const Ray& ray, const Payload& payload, glm::vec3& colorAttenuation, Ray& scattered
             ) const override;
 
-    void refract(const glm::vec3 &incident, const glm::vec3 &normal, float &ior, glm::vec3 &refracted) const;
-    glm::vec3 refract2(const glm::vec3& uv, const glm::vec3& n, float etai_over_etat) const;
-    glm::vec3 refract3(glm::vec3 i, glm::vec3 n, float eta, glm::vec3 outRay) const;
+    glm::vec3 refract(const glm::vec3& uv, const glm::vec3& n, float etai_over_etat) const;
+    void refract_2(const glm::vec3 &incident, const glm::vec3 &normal, float &ior, glm::vec3 &refracted) const;
+    glm::vec3 refract_3(glm::vec3 i, glm::vec3 n, float eta, glm::vec3 outRay) const;
 
     static double reflectance(double cosine, double ref_idx);
 
