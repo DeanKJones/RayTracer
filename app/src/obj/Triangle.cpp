5d5a8b0c55223a6774f233022b47cd5a6e98bb89
Dean <dhkjones@gmail.com>
2023-04-13 18:59:16 +0200

    Update Triangle Class

--- app/src/obj/Triangle.cpp
+++ app/src/obj/Triangle.cpp
@@ -1,64 +1,17 @@
 #include "Triangle.h"
+#include "Mesh.h"
 
 #include "imgui.h"
 #include "../Scene.h"
 #include "glm/gtc/type_ptr.hpp"
 
-Triangle::Triangle(const glm::vec3& pA, const glm::vec3& pB, const glm::vec3& pC)
-                : m_A(pA), m_B(pB), m_C(pC)
-{
-    glm::vec3 B_minus_A = pB - pA;
-    glm::vec3 C_minus_A = pC - pA;
-
-    glm::vec3 cross = glm::cross(B_minus_A, C_minus_A);
-
-    m_An = m_Bn = m_Cn = cross;
-}
-
-
-Triangle::Triangle(const glm::vec3& pA,   const glm::vec3& pB,   const glm::vec3& pC,
-                   const glm::vec3& pA_n, const glm::vec3& pB_n, const glm::vec3& pC_n)
-                        : Triangle(pA, pB, pC)
-{
-    m_An = pA_n;
-    m_Bn = pB_n;
-    m_Cn = pC_n;
-}
-
-
-Triangle::Triangle(const glm::vec3& pA,   const glm::vec3& pB,   const glm::vec3& pC,
-                   const glm::vec3& pA_n, const glm::vec3& pB_n, const glm::vec3& pC_n,
-                   const glm::vec2& pA_t, const glm::vec2& pB_t, const glm::vec2& pC_t)
-                        : Triangle(pA, pB, pC, pA_n, pB_n, pC_n)
-{
-    m_At = pA_t;
-    m_Bt = pB_t;
-    m_Ct = pC_t;
-
-    // Triangle Texture Coordinates
-    glm::vec3 edge_1 = m_B - m_A;
-    glm::vec3 edge_2 = m_C - m_A;
-
-    glm::vec2 delta_UV_1 = m_Bt - m_At;
-    glm::vec2 delta_UV_2 = m_Ct - m_At;
-
-    float det = 1.0f / ((delta_UV_1.x * delta_UV_2.y) - (delta_UV_2.x * delta_UV_1.y));
-
-    m_Tangent.x = ((delta_UV_2.y * edge_1.x) - (delta_UV_1.y * edge_2.x)) * det;
-    m_Tangent.y = ((delta_UV_2.y * edge_1.y) - (delta_UV_1.y * edge_2.y)) * det;
-    m_Tangent.z = ((delta_UV_2.y * edge_1.z) - (delta_UV_1.y * edge_2.z)) * det;
-
-    m_BiTangent.x = ((-delta_UV_2.x * edge_1.x) + (delta_UV_1.x * edge_2.x)) * det;
-    m_BiTangent.y = ((-delta_UV_2.x * edge_1.y) + (delta_UV_1.x * edge_2.y)) * det;
-    m_BiTangent.z = ((-delta_UV_2.x * edge_1.z) + (delta_UV_1.x * edge_2.z)) * det;
-}
-
-
 bool Triangle::intersect(const Ray &ray, tHit &intersector, Payload &payload) const
 {
-    const glm::vec3& v0 = m_A;
-    const glm::vec3& v1 = m_B;
-    const glm::vec3& v2 = m_C;
+    const glm::vec3& v0 = mesh->getPosition(indices[0]);
+    const glm::vec3& v1 = mesh->getPosition(indices[1]);
+    const glm::vec3& v2 = mesh->getPosition(indices[2]);
+
+    const glm::vec3 triNormal = glm::cross(v1 - v0, v2 - v0);
 
     glm::vec3 v0v1  = v1 - v0;
     glm::vec3 v0v2  = v2 - v0;
@@ -99,7 +52,7 @@ bool Triangle::intersect(const Ray &ray, tHit &intersector, Payload &payload) co
     payload.hitDistance = intersector.t_far;
     payload.hitPosition = ray.at(payload.hitDistance);
 
-    glm::vec3 normal = glm::normalize(((m_Bn * u) + (m_Cn * v) + (m_An * (1 - u - v))) / 3.0f);
+    glm::vec3 normal = glm::normalize(((triNormal * u) + (triNormal * v) + (triNormal * (1 - u - v))) / 3.0f);
     payload.setFaceNormal(ray.Direction, normal);
 
     payload.materialPtr = getMaterialPtr();
@@ -113,6 +66,10 @@ bool Triangle::intersect(const Ray &ray, tHit &intersector, Payload &payload) co
 
 bool Triangle::boundingBox(AABB &outputBox) const
 {
+    glm::vec3 m_A = mesh->getPosition(indices[0]);
+    glm::vec3 m_B = mesh->getPosition(indices[1]);
+    glm::vec3 m_C = mesh->getPosition(indices[2]);
+
     float minX = std::min(m_A.x, std::min(m_B.x, m_C.x));
     float minY = std::min(m_A.y, std::min(m_B.y, m_C.y));
     float minZ = std::min(m_A.z, std::min(m_B.z, m_C.z));
@@ -156,7 +113,7 @@ void Triangle::getUI()
     ImGui::Checkbox(": Reflection Bounces", &this->inReflections);
     ImGui::Checkbox(": Backface Culling", &this->hasBackfaceCulling);
 
-    ImGui::DragFloat3(": Vertex 0 Position", glm::value_ptr(this->m_A), 0.1f);
-    ImGui::DragFloat3(": Vertex 1 Position", glm::value_ptr(this->m_B), 0.1f);
-    ImGui::DragFloat3(": Vertex 2 Position", glm::value_ptr(this->m_C), 0.1f);
+    //ImGui::DragFloat3(": Vertex 0 Position", glm::value_ptr(mesh->getPosition(indices[0])), 0.1f);
+    //ImGui::DragFloat3(": Vertex 1 Position", glm::value_ptr(mesh->getPosition(indices[1])), 0.1f);
+    //ImGui::DragFloat3(": Vertex 2 Position", glm::value_ptr(mesh->getPosition(indices[2])), 0.1f);
 }
--- app/src/obj/Triangle.h
+++ app/src/obj/Triangle.h
@@ -8,6 +8,7 @@
 #include "Object.h"
 
 class Ray;
+class Mesh;
 
 class Triangle : public Object
 {
@@ -15,14 +16,8 @@ public:
     Triangle() : Object() {};
     ~Triangle() override = default;
 
-    Triangle(const glm::vec3& pA,   const glm::vec3& pB,   const glm::vec3& pC);
-
-    Triangle(const glm::vec3& pA,   const glm::vec3& pB,   const glm::vec3& pC,
-             const glm::vec3& pA_n, const glm::vec3& pB_n, const glm::vec3& pC_n);
-
-    Triangle(const glm::vec3& pA,   const glm::vec3& pB,   const glm::vec3& pC,
-             const glm::vec3& pA_n, const glm::vec3& pB_n, const glm::vec3& pC_n,
-             const glm::vec2& pA_t, const glm::vec2& pB_t, const glm::vec2& pC_t);
+    Triangle(const Mesh* pMesh, const std::array<uint32_t, 3>& indices, std::shared_ptr<Material>& pMaterial, bool pBackface)
+            : Object(), mesh(pMesh), indices(indices), hasBackfaceCulling(pBackface) { material_ptr = pMaterial; }
 
     bool intersect(const Ray &ray, tHit &intersector, Payload &payload) const override;
     bool boundingBox(AABB& outputBox) const override;
@@ -30,28 +25,14 @@ public:
     void getUV(const glm::vec3& p, float& u, float& v) const override {};
     void getUI() override;
 
+    //void transform(const Transform& transform);
+
 public:
     bool hasBackfaceCulling = false;
 
-public:
-    // Vertex Positions
-    glm::vec3 m_A;
-    glm::vec3 m_B;
-    glm::vec3 m_C;
-
-    // Normals
-    glm::vec3 m_An;
-    glm::vec3 m_Bn;
-    glm::vec3 m_Cn;
-
-    // Texture
-    glm::vec2 m_At;
-    glm::vec2 m_Bt;
-    glm::vec2 m_Ct;
-
-    // Calculate tangent and bi-tangent once
-    glm::vec3 m_Tangent;
-    glm::vec3 m_BiTangent;
+private:
+    const Mesh* mesh;
+    std::array<uint32_t, 3> indices;
 };
 
 
