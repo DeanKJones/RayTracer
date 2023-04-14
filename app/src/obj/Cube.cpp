815367df937c390e4bd85c5138c0e49c10ff2d3f
Dean <dhkjones@gmail.com>
2023-04-14 17:14:40 +0200

    Create inverse "cornell box" cube

--- app/src/obj/Cube.cpp
+++ app/src/obj/Cube.cpp
@@ -1,6 +1,7 @@
 
 #include "Cube.h"
 #include "imgui.h"
+#include "../shading/Lambertian.h"
 #include "glm/gtc/type_ptr.hpp"
 
 Cube::Cube(std::string pName, glm::vec3 pPosition, std::shared_ptr<Material> pMaterial,
@@ -74,6 +75,81 @@ Cube::Cube(float pSize, glm::vec3 pPosition, std::shared_ptr<Material> &pMateria
     m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
                                                 std::array<uint32_t , 3>{5, 7, 4},
                                                 pMaterial, hasBackfaceCulling));
+
+    m_BVH = std::make_shared<BVH_Node>(m_Tris, 0, m_Tris.size());
+}
+
+
+void Cube::CornellBox()
+{
+    position = {0.0f, 0.0f, 0.0f};
+    m_Size = 2;
+    // Make center by halving size
+    float halfSize = m_Size / 2;
+
+    // Create Corners
+    vertexPositions.push_back({position.x + halfSize, position.y + m_Size, position.z - halfSize});
+    vertexPositions.push_back({position.x + halfSize, position.y + m_Size, position.z + halfSize});
+    vertexPositions.push_back({position.x - halfSize, position.y + m_Size, position.z + halfSize});
+    vertexPositions.push_back({position.x - halfSize, position.y + m_Size, position.z - halfSize});
+    vertexPositions.push_back({position.x + halfSize,    position.y,          position.z - halfSize});
+    vertexPositions.push_back({position.x + halfSize,    position.y,          position.z + halfSize});
+    vertexPositions.push_back({position.x - halfSize,    position.y,          position.z + halfSize});
+    vertexPositions.push_back({position.x - halfSize,    position.y,          position.z - halfSize});
+
+    std::shared_ptr<Material> Red = std::make_shared<Lambertian>(glm::vec3(0.9f, 0.0f, 0.0f));
+    std::shared_ptr<Material> Blue = std::make_shared<Lambertian>(glm::vec3(0.0f, 0.0f, 0.9f));
+    std::shared_ptr<Material> Green = std::make_shared<Lambertian>(glm::vec3(0.0f, 0.9f, 0.0f));
+    std::shared_ptr<Material> White = std::make_shared<Lambertian>(glm::vec3(0.9f, 0.9f, 0.9f));
+
+    // Create Triangles
+    // Front
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{1, 6, 2},
+                                                Blue, hasBackfaceCulling));
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{1, 5, 6},
+                                                Blue, hasBackfaceCulling));
+
+    // Right
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{0, 5, 1},
+                                                Green, hasBackfaceCulling));
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{0, 4, 5},
+                                                Green, hasBackfaceCulling));
+
+    // Back
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{3, 4, 0},
+                                                White, hasBackfaceCulling));
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{3, 7, 4},
+                                                White, hasBackfaceCulling));
+
+    // Left
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{2, 7, 3},
+                                                Red, hasBackfaceCulling));
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{2, 6, 7},
+                                                Red, hasBackfaceCulling));
+
+    // Top
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{0, 2, 3},
+                                                White, hasBackfaceCulling));
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{0, 1, 2},
+                                                White, hasBackfaceCulling));
+
+    // Bottom
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{5, 7, 6},
+                                                White, hasBackfaceCulling));
+    m_Tris.push_back(std::make_shared<Triangle>(static_cast<const std::shared_ptr<Mesh>>(this),
+                                                std::array<uint32_t , 3>{5, 4, 7},
+                                                White, hasBackfaceCulling));
 
     m_BVH = std::make_shared<BVH_Node>(m_Tris, 0, m_Tris.size());
 }
--- app/src/obj/Cube.h
+++ app/src/obj/Cube.h
@@ -13,6 +13,7 @@ public:
             bool pVisibility, bool pInReflections, float pSize);
 
     Cube(float pSize, glm::vec3 pPosition, std::shared_ptr<Material>& pMaterial);
+    void CornellBox();
 
 public:
     bool intersect(const Ray &ray, tHit &intersector, Payload &payload) const override;
