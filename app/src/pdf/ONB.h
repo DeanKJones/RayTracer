310378b47f0e880d77c6794d2295fdad218fcbdd
Dean <dhkjones@gmail.com>
2023-04-26 16:28:23 +0200

    Make local() const

--- app/src/pdf/ONB.h
+++ app/src/pdf/ONB.h
@@ -18,7 +18,7 @@ public:
     glm::vec3 local(double a, double b, double c) const {
         return mult(a, u()) + mult(b, v()) + mult(c, w());
     }
-    glm::vec3 local(const glm::vec3& a) {
+    glm::vec3 local(const glm::vec3& a) const {
         return mult(a.x, u()) + mult(a.y, v()) + mult(a.z, w());
     }
 
@@ -31,17 +31,6 @@ private:
 
 public:
     glm::vec3 axis[3];
-
 };
-
-
-void onb::buildFromW(const glm::vec3& n)
-{
-    axis[2] = glm::normalize(n);
-    glm::vec3 a = (fabs(w().x) > 0.9) ? glm::vec3(0,1,0) : glm::vec3(1,0,0);
-    axis[1] = glm::normalize(glm::cross(w(), a));
-    axis[0] = glm::cross(w(), v());
-}
-
 
 #endif //RAYTRACING_ONB_H
